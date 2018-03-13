bcb44876e0f3058d0e6729f1a52fece03b08f512
wwdowd <35940808+wwdowd@users.noreply.github.com>
2018-03-13 11:22:57 -0700

    Add files via upload

---
+++ UnoThermobath_v1.ino
@@ -0,0 +1,29 @@
+
+//13Mar2018 WWD
+//playing around with using Arduino to read from serial communications with water bath
+//need RS-232 chip to do that
+char incomingByte;
+unsigned long milli = millis();
+
+void setup() {
+  // send data only when you receive data:
+  Serial.begin(19200);
+  }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  while (milli < 30000); {
+    milli = millis();
+    //Serial.println(milli);
+    if (Serial.available() > 0) {
+    Serial.write("RS\r");
+    // read the incoming byte:
+    incomingByte = Serial.read();
+    }
+    unsigned long milli = millis();
+    Serial.write("SS 32.00\r");
+    incomingByte = Serial.read();
+    Serial.write("RS\r");
+    incomingByte = Serial.read();
+  }
+}
